fca7a05a11fc9331df0453bd3b9459d36a4b2e7a
Julia Fidecka <julia.fidecka6@gmail.com>
2025-01-14 13:40:04 +0100

    Dodano plik z prostym skryptem JavaScript do zmiany koloru tła oraz dodano styl CSS do przycisku

---
+++ script.js
@@ -0,0 +1,7 @@
+// Alert przy załadowaniu strony
+alert("Witaj na mojej stronie!");
+
+// Funkcja zmieniająca kolor tła strony po kliknięciu przycisku
+function zmienKolorTla() {
+    document.body.style.backgroundColor = 'lightblue';
+}
